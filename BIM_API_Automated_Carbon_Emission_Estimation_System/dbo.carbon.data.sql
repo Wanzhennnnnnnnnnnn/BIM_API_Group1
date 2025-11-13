f99ee4740c7f27dc7d2fe27539f7544265eb2a99
417 <menghsun8228@gmail.com>
2025-11-13 21:21:29 +0800

    更新碳排資料表

--- BIM_API_Automated_Carbon_Emission_Estimation_System/dbo.carbon.data.sql
+++ BIM_API_Automated_Carbon_Emission_Estimation_System/dbo.carbon.data.sql
@@ -1,25 +1,43 @@
-﻿INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (1, N'預拌混凝土(280kgf/cm2, 飛灰爐石替代率30%)', N'm³', 301)
-INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (2, N'預拌混凝土(280kgf/cm2, 飛灰爐石替代率50%)', N'm³', 214)
-INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (3, N'預拌混凝土(350kgf/cm2, 飛灰爐石替代率30%)', N'm³', 341)
-INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (4, N'預拌混凝土(350kgf/cm2, 飛灰爐石替代率50%)', N'm³', 244)
-INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (5, N'預拌混凝土(140 kgf/cm2)', N'm³', 200)
-INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (6, N'預拌混凝土(175 kgf/cm2)', N'm³', 224)
-INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (7, N'預拌混凝土(210 kgf/cm2)', N'm³', 238)
-INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (8, N'預拌水中混凝土(210 kgf/cm2)', N'm³', 346)
-INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (9, N'預拌混凝土(245 kgf/cm2)', N'm³', 252)
-INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (10, N'預拌混凝土(280 kgf/cm2)', N'm³', 338)
-INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (11, N'自充填預拌混凝土(350 kgf/cm2)', N'm³', 375)
-INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (12, N'卜特蘭水泥(II型)', N'kg', 0.981)
-INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (13, N'卜特蘭水泥(II型)', N'kg', 0.964)
-INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (14, N'鋁質水泥', N'kg', 10.1)
-INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (15, N'水泥熟料', N'kg', 0.948)
-INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (16, N'水泥(不分型號)', N'kg', 0.907)
-INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (17, N'水泥熟料', N'kg', 0.95)
-INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (18, N'卜特蘭水泥(乾式)', N'kg', 0.94)
-INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (19, N'鋼筋混凝土用鋼筋(SD280W)', N'kg', 0.835)
-INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (20, N'鋼筋混凝土用鋼筋(SD420W)', N'kg', 0.834)
-INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (21, N'牆面石材', N'm²', 15.11)
-INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (22, N'牆面磁磚', N'm²', 19.1)
-INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (23, N'地坪類磁磚', N'm²', 25.16)
-INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (24, N'地坪類實木地板', N'm²', 9.08)
-INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (25, N'裝修玻璃 - 合成', N'kg', 0.75318)
+﻿INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (1,  N'預拌混凝土(280kgf/cm2, 飛灰爐石替代率30%)', N'm³', 301);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (2,  N'預拌混凝土(280kgf/cm2, 飛灰爐石替代率50%)', N'm³', 214);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (3,  N'預拌混凝土(350kgf/cm2, 飛灰爐石替代率30%)', N'm³', 341);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (4,  N'預拌混凝土(350kgf/cm2, 飛灰爐石替代率50%)', N'm³', 244);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (5,  N'預拌混凝土(140 kgf/cm2)', N'm³', 200);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (6,  N'預拌混凝土(175 kgf/cm2)', N'm³', 224);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (7,  N'預拌混凝土(210 kgf/cm2)', N'm³', 238);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (8,  N'預拌水中混凝土(210 kgf/cm2)', N'm³', 346);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (9,  N'預拌混凝土(245 kgf/cm2)', N'm³', 252);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (10, N'預拌混凝土(280 kgf/cm2)', N'm³', 338);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (11, N'自充填預拌混凝土(350 kgf/cm2)', N'm³', 375);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (12, N'卜特蘭水泥(II型)', N'kg', 0.981);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (13, N'卜特蘭水泥(II型)', N'kg', 0.964);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (14, N'鋁質水泥', N'kg', 10.1);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (15, N'水泥熟料', N'kg', 0.948);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (16, N'水泥(不分型號)', N'kg', 0.907);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (17, N'水泥熟料', N'kg', 0.95);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (18, N'卜特蘭水泥(乾式)', N'kg', 0.94);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (19, N'牆面石材', N'm²', 15.11);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (20, N'牆面磁磚', N'm²', 19.1);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (21, N'地坪類磁磚', N'm²', 25.16);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (22, N'地坪類實木地板', N'm²', 9.08);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (23, N'裝修玻璃 - 合成', N'kg', 0.75318);
+
+-- SD280W #3~#10
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (24, N'鋼筋混凝土用鋼筋(SD280W)-#3',  N'm', 0.467328);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (25, N'鋼筋混凝土用鋼筋(SD280W)-#4',  N'm', 0.832149);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (26, N'鋼筋混凝土用鋼筋(SD280W)-#5',  N'm', 1.304048);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (27, N'鋼筋混凝土用鋼筋(SD280W)-#6',  N'm', 1.881998);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (28, N'鋼筋混凝土用鋼筋(SD280W)-#7',  N'm', 2.535097);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (29, N'鋼筋混凝土用鋼筋(SD280W)-#8',  N'm', 3.330956);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (30, N'鋼筋混凝土用鋼筋(SD280W)-#9',  N'm', 4.223974);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (31, N'鋼筋混凝土用鋼筋(SD280W)-#10', N'm', 5.279902);
+
+-- SD420W #3~#10
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (32, N'鋼筋混凝土用鋼筋(SD420W)-#3',  N'm', 0.466768);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (33, N'鋼筋混凝土用鋼筋(SD420W)-#4',  N'm', 0.831148);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (34, N'鋼筋混凝土用鋼筋(SD420W)-#5',  N'm', 1.302667);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (35, N'鋼筋混凝土用鋼筋(SD420W)-#6',  N'm', 1.878922);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (36, N'鋼筋混凝土用鋼筋(SD420W)-#7',  N'm', 2.531735);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (37, N'鋼筋混凝土用鋼筋(SD420W)-#8',  N'm', 3.327147);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (38, N'鋼筋混凝土用鋼筋(SD420W)-#9',  N'm', 4.219530);
+INSERT INTO [dbo].[carbon] ([Id], [name], [unit], [kgCO2e]) VALUES (39, N'鋼筋混凝土用鋼筋(SD420W)-#10', N'm', 5.275289);
